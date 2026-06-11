eba07190fc171977f64aae851178ebd84076bdaf
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2026-06-11 14:49:11 -0300

    Add: design-browser-history - submission-0

---
+++ Data Structures & Algorithms/design-browser-history/submission-0.py
@@ -0,0 +1,33 @@
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.history = [homepage]
+        self.index = 0
+
+    def visit(self, url: str) -> None:
+        while self.index + 1 < len(self.history):
+            self.history.pop()
+
+        self.history.append(url)
+        self.index += 1
+
+    def back(self, steps: int) -> str:
+        if self.index - steps >= 0:
+            self.index -= steps
+        else:
+            self.index = 0
+        return self.history[self.index] 
+
+    def forward(self, steps: int) -> str:
+        if self.index + steps < len(self.history):
+            self.index += steps
+        else:
+            self.index = len(self.history) - 1
+        return self.history[self.index]
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
