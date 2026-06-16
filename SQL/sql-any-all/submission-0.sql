4eb6a63285ffb9580827fa637770c5ba9022df18
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2026-06-16 11:16:44 -0300

    Add: sql-any-all - submission-0

---
+++ SQL/sql-any-all/submission-0.sql
@@ -0,0 +1,49 @@
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name TEXT
+);
+
+CREATE TABLE orders (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    customer_id INTEGER,
+    price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 300),
+  (7, 70),
+  (8, 400);
+-- Do not modify above this line. --
+
+SELECT name
+FROM customers c
+WHERE c.id = ANY (
+    SELECT customer_id
+    FROM orders o
+    WHERE o.price < 100 and o.customer_id = c.id
+)
+ORDER BY name ASC;
+
+
+
+
+
+
+
+
+
